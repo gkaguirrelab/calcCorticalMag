d2db7afe90728ccac4c85a1688a1ef0dca99602a
gkaguirre <gkaguirre@me.com>
2019-10-16 14:39:27 -0400

    Still developing
    Signed-off-by: gkaguirre <gkaguirre@me.com>

--- code/calcCorticalMag.m
+++ code/calcCorticalMag.m
@@ -18,7 +18,7 @@ p.addRequired('inferredMapsDirPath',@isstr);
 p.addRequired('surfPath',@isstr);
 
 % Optional key-value pairs
-p.addParameter('hemisphere','lh',@ischar);
+p.addParameter('hemisphere','rh',@ischar);
 p.addParameter('whichSurface','inflated',@ischar); % pial, white, or sphere
 p.addParameter('stepSize',1,@isscalar);
 p.addParameter('angleSet',10:40:170,@isnumeric);
@@ -30,10 +30,14 @@ p.addParameter('areaSet',[1],@isnumeric);
 p.parse(inferredMapsDirPath, surfPath, varargin{:})
 
 % Pull some variables out of the param parser results
-stepSize = p.Results.stepSize;
+initialStepSize = p.Results.stepSize;
 areaSet = p.Results.areaSet;
 angleSet = p.Results.angleSet;
 
+% The polar angle values will be negative in the right hemisphere
+if strcmp(p.Results.hemisphere,'rh')
+%    angleSet = -angleSet;
+end
 
 %% Load surface files
 surfName = fullfile(surfPath,[p.Results.hemisphere '.' p.Results.whichSurface]);
@@ -93,6 +97,10 @@ for areaIdx = 1:length(areaSet)
         % Re-initialize the distanceMap with the start map
         distanceMap = distanceMapStart;
         
+        % Set the step size back to the initial value
+        stepSize = initialStepSize;
+        targetAngle = angleSet(angleIdx);
+        
         %% Perform the path search
         % This is about four minutes on a single laptop core for
         %  stepSize = 1;
@@ -102,19 +110,23 @@ for areaIdx = 1:length(areaSet)
             % Iterate the position counter
             positionIdx = positionIdx+1;
             
-            % Anonymous function that provides an error metric
-            myObj = @(x) (areaAngleMap-x).^2 + 100.*(distanceMap-stepSize).^2;
+            % Calculate an error metric
+            errorMap = (areaAngleMap-targetAngle).^2 + 10.*(distanceMap-stepSize).^2;
             
             % Minimize the error and get the point
-            [fVal, idx] = ...
-                min(myObj(angleSet(angleIdx)));
-            
-            % If we have an infinite error value, we have run out of visual
-            % area
-            if isinf(fVal)
+            [fVal, idx] =  min(errorMap);
+
+            % Handle the output cases
+            if fVal > 1000
+                % We have an infinite error (or effectively infinite)
+                % which means we have run out of visual area. Time to stop.
                 stillWorkingFlag = false;
+            elseif fVal > 50
+                % We can't find a good next point. Try again with a bigger
+                % step size.
+                positionIdx = positionIdx-1;
+                stepSize = stepSize + initialStepSize;                
             else
-                
                 % Store the values
                 path(positionIdx,angleIdx,areaIdx) = idx;
                 pathPositions(positionIdx,angleIdx,areaIdx) = ...
@@ -128,22 +140,73 @@ for areaIdx = 1:length(areaSet)
                 distanceMap(vareaMap~=areaIdx)=Inf;
                 
                 % Salt the earth that we have already crossed
-                distanceMap(distanceMapStart<((positionIdx-1)*stepSize))=Inf;
-                
+                distanceMap(distanceMapStart<=((positionIdx-1)*stepSize))=Inf;                
+
+                % Re-set the stepSize in case it was changed
+                stepSize = initialStepSize;
             end
             
         end % Loop over positions
     end % Loop over angle
 end % Loop over visual areas
 
-% Fit an exponential function to the eccentricity values
-myExp = fittype( @(a,b,c, x) c.* (a.^(x./max(x))-b) );
-y = eccenMap(path(:,angleIdx,areaIdx));
-x = pathPositions(:,angleIdx,areaIdx);
-myFit = fit(x,y,myExp);
-yFit = myFit(x);
 
-% This is inverse M
-plot(yFit(1:end-1),diff(yFit)./diff(x))
+%% Obtain slope of magnification function per visual area
+for areaIdx = 1:length(areaSet)
+    p1 = nan(length(areaSet));
+    p2 = nan(length(areaSet));
+    for angleIdx = 1:length(angleSet)
+        y = eccenMap(path(:,angleIdx,areaIdx));
+        x = pathPositions(:,angleIdx,areaIdx);
+        degPerMm = diff(y)./diff(x);
+        lastIdx = find(y>10);
+        lastIdx = lastIdx(1);
+        myFit = fit(y(1:lastIdx),degPerMm(1:lastIdx),'poly1');
+        plot(y(1:lastIdx),degPerMm(1:lastIdx),'x');
+        hold on
+        plot(y(1:lastIdx),myFit(y(1:lastIdx)),'-');
+        p1(angleIdx)=myFit.p1;
+        p2(angleIdx)=myFit.p2;
+    end    
+end
+
 
 end % Main function
+
+%% LOCAL FUNCTIONS
+
+function plotSurfaceMap(vert, face, srf)
+
+mycolormap = flipud(jet(200));
+validIdx = logical(double(~isnan(srf)) .* double(~isinf(srf)));
+myvec = linspace(nanmin(srf(validIdx)),nanmax(srf(validIdx)),200);
+
+cmap_vals = repmat(zeros(size(srf))+0.5,1,3);
+
+for ii = 1:length(srf)
+    % Find the closest color value to the srf(ii) value
+    [~,ind] = min(abs(myvec-srf(ii)));
+    if isinf(srf(ii)) || srf(ii)==0
+        col4thisvox = [.5 .5 .5]; % set nan and zero to gray
+    else
+        col4thisvox = mycolormap(ind,:);
+    end
+    cmap_vals(ii,:) = col4thisvox;
+end
+
+brain.vertices = vert;
+brain.faces = face;
+brain.facevertexcdata = cmap_vals;
+
+% Brain surface
+patch(brain,'EdgeColor','none','facecolor','interp','FaceAlpha',1);
+daspect([1 1 1]);
+
+% Camera settings
+camproj perspective; % orthographic; perspective
+lighting phong; % flat; gouraud; phong
+material dull; % shiny; metal; dull
+camlight('headlight');
+
+
+end
